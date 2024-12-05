5b01b5523a9e83fefc65f6a87f38cdda35a700ba
Gin <181115100+bogfrog37@users.noreply.github.com>
2024-12-04 20:02:26 -0500

    beep

---
+++ jquery.js
@@ -0,0 +1,8 @@
+function alertHover(){
+  $("#area").css("background-color", "green");
+  alert("Close me to turn the box green.");
+}
+
+function reset(){
+   $("#area").css("background-color", "#953674");
+}
